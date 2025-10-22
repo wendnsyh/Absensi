b71afb518b2635ba6e327386a0ee5e26b6d8e958
najdahh <najdahh.isa@gmail.com>
2025-10-22 17:36:49 +0700

    Menambahkan fitur cuaca terkini dengan integrasi API Open-Meteo pada controller Absensi, Admin, dan Pegawai. Memperbarui logika pengambilan data cuaca dan menampilkan informasi suhu, kecepatan angin, kelembapan, serta waktu terbit dan terbenamnya matahari.

--- application/controllers/Pegawai.php
+++ application/controllers/Pegawai.php
@@ -23,6 +23,44 @@ class Pegawai extends CI_Controller
 
     public function index()
     {
+        $latitude = -6.3452; // Koordinat Kecamatan Setu, Tangerang Selatan
+        $longitude = 106.6725;
+        $api_url = "https://api.open-meteo.com/v1/forecast?latitude={$latitude}&longitude={$longitude}&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&daily=sunrise,sunset&timezone=Asia%2FJakarta";
+
+        $weather_data = json_decode(file_get_contents($api_url), true);
+        if ($weather_data && isset($weather_data['current'])) {
+            $data['temperature'] = $weather_data['current']['temperature_2m'];
+            $data['wind_speed'] = $weather_data['current']['wind_speed_10m'];
+            $data['humidity'] = $weather_data['current']['relative_humidity_2m'];
+            $data['weather_code'] = $weather_data['current']['weather_code'];
+            $data['update_time'] = date('d M Y H:i', strtotime($weather_data['current']['time']));
+            $data['sunrise'] = date('H:i', strtotime($weather_data['daily']['sunrise'][0]));
+            $data['sunset'] = date('H:i', strtotime($weather_data['daily']['sunset'][0]));
+        } else {
+            $data['temperature'] = '-';
+            $data['wind_speed'] = '-';
+            $data['humidity'] = '-';
+            $data['weather_code'] = '-';
+            $data['sunrise'] = '-';
+            $data['sunset'] = '-';
+            $data['last_update'] = '-';
+        }
+        $weather_codes = [
+            0 => 'Cerah',
+            1 => 'Cerah Berawan',
+            2 => 'Berawan',
+            3 => 'Mendung',
+            45 => 'Kabut',
+            48 => 'Kabut Beku',
+            51 => 'Gerimis Ringan',
+            61 => 'Hujan Ringan',
+            63 => 'Hujan Sedang',
+            65 => 'Hujan Lebat',
+            80 => 'Hujan Lokal',
+            95 => 'Badai Petir'
+        ];
+        $data['weather_text'] = $weather_codes[$data['weather_code']] ?? 'Tidak Diketahui';
+
         $this->load->library('pagination');
 
         $keyword = $this->input->get('keyword'); // ambil keyword dari form GET
